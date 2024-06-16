54d2411f00405478a5b786c71bf0e38a193f717e
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-06-16 13:06:58 +0530

    All dependencies added which are needed for the website

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose')
+require("dotenv/config")
+const app = express();
+// cross origin resource sharing
+const cors = require('cors');
+
+app.use(cors({ origin: true }));
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    return res.json("Hey there");
+})
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true })
+mongoose.connection.once("open", () => {
+    console.log("Connected")
+}).on("error", (error) => {
+    console.log(`Error: ${error}`)
+})
+
+
+app.listen(4000, () => {
+    console.log("listening to port 4000")
+})
